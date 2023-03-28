c3d5d48e7e2c03b741e2eb4f6f6a53fa7d72d864
LuisCastilloGD <lcastillo@griddynamics.com>
2023-03-28 12:43:05 -0600

    add eureka client

--- src/main/java/com/grid/inventory/InventoryApplication.java
+++ src/main/java/com/grid/inventory/InventoryApplication.java
@@ -2,9 +2,10 @@ package com.grid.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class InventoryApplication {
 
 	public static void main(String[] args) {
